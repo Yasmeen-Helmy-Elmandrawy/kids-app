f7db935555d18d4d76a06028324a3f0c7c56d008
Yasmeen-Helmy-Elmandrawy <yasmeenhelmy2020@gmail.com>
2022-07-18 02:19:42 +0200

    first

--- src/App.js
+++ src/App.js
@@ -4,19 +4,7 @@ import Home from './components/Home';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
 import Profile from './components/Profile';
-import Objects from './components/Objects';
 import ForgetPassword from './components/ForgetPassword';
-import ResetPassword from './components/ResetPassword';
-import BasicVideo from './components/BasicVideo';
-import SequenceVideo from './components/SequenceVideo';
-import LoopVideo from './components/LoopVideo';
-import FunctionVideo from './components/FunctionVideo';
-import ArrayVideo from './components/ArrayVideo';
-import BasicExplain from './components/BasicExplain';
-import SequenceExplain from './components/SequenceExplain';
-import LoopExplain from './components/LoopExplain';
-import FunctionExplain from './components/FunctionExplain';
-import ArrayExplain from './components/ArrayExplain';
 const App = () => {
   return ( 
     <BrowserRouter>
@@ -25,19 +13,7 @@ const App = () => {
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
         <Route path="/profile" component={Profile} />
-        <Route path="/objects" component={Objects} />
         <Route path="/forget" component={ForgetPassword} />
-        <Route path="/reset" component={ResetPassword} />
-        <Route path="/basic-v" component={BasicVideo} />
-        <Route path="/sequence-v" component={SequenceVideo} />
-        <Route path="/loop-v" component={LoopVideo} />
-        <Route path="/function-v" component={FunctionVideo} />
-        <Route path="/array-v" component={ArrayVideo} />
-        <Route path="/basic-e" component={BasicExplain} />
-        <Route path="/sequence-e" component={SequenceExplain} />
-        <Route path="/loop-e" component={LoopExplain} />
-        <Route path="/function-e" component={FunctionExplain} />
-        <Route path="/array-e" component={ArrayExplain} />
       </div>
     </BrowserRouter>
    );
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import {Link} from 'react-scroll';
 import { Link as NavLink } from 'react-router-dom';
 import { CgGames } from "react-icons/cg";
-import { FiEdit } from "react-icons/fi";
-import { FiUsers } from "react-icons/fi";
+import { MdLiveHelp } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
 import { MdOutlineRateReview } from "react-icons/md";
 import { FiLogOut } from "react-icons/fi";
 import { FaArrowCircleLeft } from 'react-icons/fa';
 import './style.css';
 const Profile = () => {
-    const [t, i18n] = useTranslation();
     return ( 
         <div className="profile">
             <div className="header">
-                <div className="user">
-                    <img src="./images/avatar.png" alt="avatar-profile" />
-                    <h3 className="name">yasmeen helmy</h3>
-                </div>
+                <h2 className="name">Kidcoder</h2>
                 <nav className="navbar">
                     <ul>
                         <li><Link to="games"><CgGames className="icon" />Games</Link></li>
-                        <li><Link to="edit-profile"><FiEdit className="icon" />Edit profile</Link></li>
-                        <li><Link to="about"><FiUsers className="icon" />About Us</Link></li>
+                        <li><Link to="help"><MdLiveHelp className="icon" />Help Center</Link></li>
+                        <li><Link to="about"><FaUsers className="icon" />About Us</Link></li>
                         <li><Link to="rate"><MdOutlineRateReview className="icon" />Rate Us</Link></li>
                         <li><NavLink className="back" to="/signin"><FiLogOut className="icon" />Log Out</NavLink></li>
                     </ul>
@@ -46,7 +41,7 @@ const Profile = () => {
                         <div className="gametype">
                             <div className="gametype-contain">
                                 <img src="./images/loop/loop.png" alt="loops" />
-                                <Link to = "loops" className="link-loop">loops</Link>
+                                <Link to = "loops" className="link-loop">Loops</Link>
                             </div>
                         </div>
                     </div>
@@ -79,7 +74,7 @@ const Profile = () => {
                             </div>
                             <div className="content">
                                 <img src="./images/basic/code-basic.png" alt="explain-basic" />
-                                <Link to = "explain-basic" className="link2">Explain</Link>
+                                <Link to = "explain-basic" className="link2">Coding</Link>
                             </div>
                             <div className="content">
                                 <img src="./images/basic/basic4.jpg" alt="img-basic" />
@@ -103,10 +98,23 @@ const Profile = () => {
                 <div className="explain-basic e-basicColor">
                     <div className="title">
                         <Link to="basics" className="back"><FaArrowCircleLeft /></Link>
-                        <h2>Explain</h2>
+                        <h2>Coding</h2>
                     </div>
                     <div className="explain">
-                        <p>Explain</p>
+                        <div className="definition-basic">
+                            <img className="img-basic" src="./images/basic/basic.png" alt="icon-basics" />
+                            <p className="para-basic">Computer coding is the use of computer programming languages to give 
+                            computers and machines a set of instructions on what actions to perform.</p>
+                        </div>
+                        <h2 className="h2-basic">Basic Coding Concepts</h2>
+                        <h3 className="h3-basic">-Variable</h3>
+                        <p className="para2-basic">A variable is a name of the memory location. It is used to store data. 
+                        Its value can be changed, and it can be reused many times.</p>
+                        <p className="para2-basic var">The example of declaring the variable is given below:</p>
+                        <img className="img-code" src="./images/basic/code.png" alt="example-code" /><br />
+                        <p className="para2-basic">Here, a, b, c are variables. The int, float, char are the data types.</p>
+                        <p className="para2-basic var">We can also provide values while declaring the variables as given below:</p>
+                        <img className="img-code" src="./images/basic/code2.png" alt="example-code-2" /><br />
                     </div>
                 </div>
 
@@ -145,7 +153,7 @@ const Profile = () => {
                             </div>
                             <div className="content">
                                 <img src="./images/sequence/code-sequence.jpg" alt="explain-sequence" />
-                                <Link to = "explain-sequence" className="link2">Explain</Link>
+                                <Link to = "explain-sequence" className="link2">Coding</Link>
                             </div>
                             <div className="content">
                                 <img src="./images/sequence/sequence2.jpg" alt="games-sequence" />
@@ -161,7 +169,7 @@ const Profile = () => {
                         <h2>Video Tutorial</h2>
                     </div>
                     <video className="video" controls autoPlay loop>
-                        <source src="./videos/Basic.mp4" type="video/mp4" />
+                        <source src="./videos/Sequence.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
                 </div>
@@ -169,10 +177,21 @@ const Profile = () => {
                 <div className="explain-sequence e-sequenceColor">
                     <div className="title">
                         <Link to="sequences" className="back"><FaArrowCircleLeft /></Link>
-                        <h2>Explain</h2>
+                        <h2>Coding</h2>
                     </div>
                     <div className="explain">
-                        <p>Explain</p>
+                        <div className="definition-sequence">
+                            <img className="img-sequence" src="./images/sequence/img-sequence.png" alt="icon-sequnces" />
+                            <p className="para-sequence">A sequence in programming refers to an ordered set of instructions or tasks.
+                            Here the command is executed exactly in the same order of events given by the coder.</p>
+                        </div>
+                        <p className="para2-sequence">Fibonacci Series in C: In case of fibonacci series, next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21 etc. 
+                        The first two numbers of fibonacci series are 0 and 1.</p>
+                        <p className="para2-sequence understand">To understand the sequence point more detail, you can consider the following given examples:</p>
+                        <div className="code">
+                            <img className="img-code" src="./images/sequence/code.png" alt="example-code" />
+                            <img className="img-output" src="./images/sequence/output.png" alt="output-code" />
+                        </div>
                     </div>
                 </div>
 
@@ -211,7 +230,7 @@ const Profile = () => {
                             </div>
                             <div className="content">
                                 <img src="./images/loop/code-loop.jpg" alt="explain-sequence" />
-                                <Link to = "explain-loop" className="link2">Explain</Link>
+                                <Link to = "explain-loop" className="link2">Coding</Link>
                             </div>
                             <div className="content">
                                 <img src="./images/loop/loops.png" alt="games-sequence" />
@@ -235,10 +254,23 @@ const Profile = () => {
                 <div className="explain-loop e-loopColor">
                     <div className="title">
                         <Link to="loops" className="back"><FaArrowCircleLeft /></Link>
-                        <h2>Explain</h2>
+                        <h2>Coding</h2>
                     </div>
                     <div className="explain">
-                        <p>Explain</p>
+                        <div className="definition">
+                            <img className="img-loop" src="./images/loop/loop.png" alt="icon-loops" />
+                            <p className="para-loop">A programming structure that repeats a sequence of instructions 
+                            until a specific condition is met. Programmers use loops to cycle through values, 
+                            add sums of numbers, repeat functions, and many other things.</p>
+                        </div>
+                        <h3 className="h3-loop">For Loop</h3>
+                        <p className="para2-loop">A for loop is similar to a while loop, but streamlines the source code. 
+                        The for loop statement defines the start and end point as well as the increment for each iteration. 
+                        </p>
+                        <div className="code-screen">
+                            <img className="img-code" src="./images/loop/code2.png" alt="example-code-2" />
+                            <img className="img-output" src="./images/loop/output.png" alt="output-code-2" />
+                        </div>
                     </div>
                 </div>
 
@@ -277,7 +309,7 @@ const Profile = () => {
                             </div>
                             <div className="content">
                                 <img src="./images/function/code-function.png" alt="explain-sequence" />
-                                <Link to = "explain-function" className="link2">Explain</Link>
+                                <Link to = "explain-function" className="link2">Coding</Link>
                             </div>
                             <div className="content">
                                 <img src="./images/function/function2.png" alt="games-sequence" />
@@ -301,10 +333,20 @@ const Profile = () => {
                 <div className="explain-function e-functionColor">
                     <div className="title">
                         <Link to="functions" className="back"><FaArrowCircleLeft /></Link>
-                        <h2>Explain</h2>
+                        <h2>Coding</h2>
                     </div>
                     <div className="explain">
-                        <p>Explain</p>
+                        <div className="definition-f">
+                            <img className="img-function" src="./images/function/function.png" alt="icon-functions" />
+                            <p className="para-function">A function is a set of statements that take inputs, 
+                            do some specific computation and produces output.</p>
+                        </div>
+                        <p className="example">Given below is the source code for a function called max(). 
+                        This function takes two parameters x and y and returns the maximum value between the two</p>
+                        <div className="code">
+                            <img className="img-code" src="./images/function/code.png" alt="example-code" />
+                            <img className="img-output" src="./images/function/output.png" alt="output-code" />
+                        </div>
                     </div>
                 </div>
 
@@ -343,7 +385,7 @@ const Profile = () => {
                             </div>
                             <div className="content">
                                 <img src="./images/array/code-array.png" alt="explain-array" />
-                                <Link to = "explain-array" className="link2">Explain</Link>
+                                <Link to = "explain-array" className="link2">Coding</Link>
                             </div>
                             <div className="content">
                                 <img src="./images/array/array.png" alt="games-array" />
@@ -367,10 +409,20 @@ const Profile = () => {
                 <div className="explain-array e-arrayColor">
                     <div className="title">
                         <Link to="arrays" className="back"><FaArrowCircleLeft /></Link>
-                        <h2>Explain</h2>
+                        <h2>Coding</h2>
                     </div>
                     <div className="explain">
-                        <p>Explain</p>
+                        <div className="definition-array">
+                            <img className="img-array" src="./images/array/arrays.png" alt="icon-arrays" />
+                            <p className="para-array">Arrays are used to store multiple values in a single variable, 
+                            instead of declaring separate variables for each value.
+                            To create an array, define the data type (like int) and specify the name of the array followed by square brackets [].</p>
+                        </div>
+                        <h2 className="h2-array">Access the Elements of an Array</h2>
+                        <p className="para2-array">To access an array element, refer to its index number.
+                        Array indexes start with 0: [0] is the first element. [1] is the second element, etc.</p>
+                        <h3 className="h3-array">This statement accesses the value of the first element [0]</h3>
+                        <img className="img-code" src="./images/array/example-array.png" alt="example-code" />
                     </div>
                 </div>
 
@@ -396,12 +448,14 @@ const Profile = () => {
                     </div>
                 </div>
 
-                <div className="edit-profile" id="edit-profile">
+                <div className="help" id="help">
                     <div className="container">
-                        <form>
-                            <input type = "text" placeholder = "Enter name..." id = "name" />
-                            <input type = "submit" value = "Add" />
-                        </form>
+                        <h2>What is this App For?</h2>
+                        <img src="./images/help-center.png" alt="help" />
+                        <p>This application aims to learn programming to kids and take advantage of their time and develop their knowledge and talents.
+                        Our app helps kids improve their problem solving skills by playing different games like basics of coding, sequences, loops, functions and arrays.
+                        It also helps kids to enhance memory and increase logical thinking skills.
+                        </p>
                     </div>
                 </div>
 
@@ -411,11 +465,11 @@ const Profile = () => {
                         <div className="users">
                             <div className="user">
                                 <img src="./images/yasmeen.png" alt="user1-img" />
-                                <h3>katreen mikhael<br /><span>(FrontEnd Developer)</span></h3>
+                                <h3>yasmeen helmy<br /><span>(FrontEnd Developer)</span></h3>
                             </div>
                             <div className="user">
-                                <img src="./images/yasmeen.png" alt="user2-img" />
-                                <h3>yasmeen helmy<br /><span>(FrontEnd Developer)</span></h3>
+                                <img src="./images/katreen.jpg" alt="user2-img" />
+                                <h3>katreen mikhael<br /><span>(FrontEnd Developer)</span></h3>
                             </div>
                             <div className="user">
                                 <img src="./images/yasmeen.png" alt="user3-img" />
@@ -424,11 +478,11 @@ const Profile = () => {
                         </div>
                         <div className="users">
                             <div className="user">
-                                <img src="./images/yasmeen.png" alt="user4-img" />
+                                <img src="./images/kerollos.jpg" alt="user4-img" />
                                 <h3>kerollos romany<br /><span>(Flutter Developer)</span></h3>
                             </div>
                             <div className="user">
-                                <img src="./images/yasmeen.png" alt="user5-img" />
+                                <img src="./images/fatma1.jpg" alt="user5-img" />
                                 <h3>fatma radwan<br /><span>(Flutter Developer)</span></h3>
                             </div>
                         </div>

4a759cb4b7d86f3bc9180d71ae7a2754f315f11b
Yasmeen-Helmy-Elmandrawy <yasmeenhelmy2020@gmail.com>
2022-07-20 16:08:40 +0200

    fifth

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,80 +1,100 @@
-import React, {useState, useEffect} from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
-import { useHistory } from 'react-router-dom';
-import './style.css';
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { useHistory } from "react-router-dom";
+import "./style.css";
 const SignIn = () => {
-    const [t, i18n] = useTranslation();
-    const [passwordEye, setPasswordEye] = useState(false);
-    const handlePasswordClick = () => {
-        setPasswordEye(!passwordEye)
-    }
+  const [t, i18n] = useTranslation();
+  const [passwordEye, setPasswordEye] = useState(false);
+  const handlePasswordClick = () => {
+    setPasswordEye(!passwordEye);
+  };
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [login, setLogin]= useState(false);
-    const history = useHistory();
-    const handleLogin = async () => {
-        let data = await fetch("https://kidcoder.herokuapp.com/logIn",{
-            method:'POST',
-            body: JSON.stringify({email: email, pwd: password}),
-            headers: {
-                ACCESS_TOKEN_SECRET: 
-                    "0f5cce9faab589e2e5819c498262907ed7a9729953eee56e0d32645693c2d4df0a61314b7d2f1fe354827e4c8b1984b19bb1c7fd8558e67aa2b2a74353b59ef0",
-                    'Content-Type': 'application/json',
-            },   
-        })
-        data = await data.json();
-        console.log(data);
-        setLogin(true);
-           
-    }
-    useEffect(() => {
-        if (login){
-            history.push("/profile");
-        }
-    });
-    console.log(email);
+  const [emaill, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    console.log(emaill);
     console.log(password);
-       
-    return ( 
-        <div className="login">
-            <img className="mascot-doc" src="./images/mascot-doc.png" alt="mascot-doc" />
-            <form className="input-group2 signin">
-                <label className="label2">{t('username')}</label>
-                <input 
-                    type="text" 
-                    className="input-field2"
-                    placeholder={t('enter-username')}
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    id="input_1" 
-                    required 
-                />
-                <label className="label2">{t('password')}</label>
-                <input 
-                    type={passwordEye === false ? "password": "text"}
-                    className="input-field2"
-                    placeholder={t('enter-password')}
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    id="input_2"
-                    required
-                />
-                <div className="icon-eye">
-                    {
-                        (passwordEye === false)?<AiFillEyeInvisible onClick={handlePasswordClick} />:
-                        <AiFillEye onClick={handlePasswordClick} />
-                    }  
-                </div>
-                <input type="checkbox" className="check-box" /><span>{t('remember')}</span>
-                <Link to="/forget">{t('forget')}</Link>
-                <button type="submit" className="submit-button btn2" onClick={handleLogin}>{t('signin')}</button>
-                <p>{t('account')}<Link to="/signup" className="link">{t('signup')}</Link></p>   
-            </form>
+    const data = await fetch("https://kidcoder.herokuapp.com/logIn", {
+      method: "POST",
+      body: JSON.stringify({
+        email: emaill,
+        pwd: password,
+      }),
+      headers: {
+        ACCESS_TOKEN_SECRET:
+          "0f5cce9faab589e2e5819c498262907ed7a9729953eee56e0d32645693c2d4df0a61314b7d2f1fe354827e4c8b1984b19bb1c7fd8558e67aa2b2a74353b59ef0",
+        "Content-Type": "application/json",
+      },
+    });
+    const res = await data.json();
+    console.log(data);
+    if (res.status === "success") {
+      console.log("success");
+      history.push("/profile");
+    }
+    if (res.status === "error") {
+      console.log("error");
+    }
+  };
+
+  return (
+    <div className="login">
+      <img
+        className="mascot-doc"
+        src="./images/mascot-doc.png"
+        alt="mascot-doc"
+      />
+      <form className="input-group2 signin">
+        <label className="label2">{t("username")}</label>
+        <input
+          type="text"
+          className="input-field2"
+          placeholder={t("enter-username")}
+          onChange={(e) => setEmail(e.target.value)}
+          value={emaill}
+          id="input_1"
+          required
+        />
+        <label className="label2">{t("password")}</label>
+        <input
+          type={passwordEye === false ? "password" : "text"}
+          className="input-field2"
+          placeholder={t("enter-password")}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          id="input_2"
+          required
+        />
+        <div className="icon-eye">
+          {passwordEye === false ? (
+            <AiFillEyeInvisible onClick={handlePasswordClick} />
+          ) : (
+            <AiFillEye onClick={handlePasswordClick} />
+          )}
         </div>
-     );
-}
- 
+        <input type="checkbox" className="check-box" />
+        <span>{t("remember")}</span>
+        <Link to="/forget">{t("forget")}</Link>
+        <button
+          type="submit"
+          className="submit-button btn2"
+          onClick={handleLogin}
+        >
+          {t("signin")}
+        </button>
+        <p>
+          {t("account")}
+          <Link to="/signup" className="link">
+            {t("signup")}
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
 export default SignIn;
--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,132 +1,159 @@
-import React, {useState} from 'react';
-import { useTranslation } from 'react-i18next';
-import {Link} from 'react-router-dom';
-import {useForm} from "react-hook-form";
-import { AiFillEyeInvisible, AiFillEye } from 'react-icons/ai';
-import { useHistory } from 'react-router-dom';
-import './style.css';
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { useHistory } from "react-router-dom";
+import "./style.css";
 const SignUp = () => {
-    const [t, i18n] = useTranslation();
-    const {register, handleSubmit, formState: { errors}} = useForm();
-    const onSubmit = (data) => {
-        console.log(data);
-    }
-    console.log(errors);
+  const [t, i18n] = useTranslation();
+  const {
+    register,
+    formState: { errors },
+  } = useForm();
+  // const onSubmit = (data) => {
+  //     console.log(data);
+  // }
+  console.log(errors);
 
-    const [passwordEye, setPasswordEye] = useState(false);
-    const handlePasswordClick = () => {
-        setPasswordEye(!passwordEye)
-    }
+  const [passwordEye, setPasswordEye] = useState(false);
+  const handlePasswordClick = () => {
+    setPasswordEye(!passwordEye);
+  };
 
-    const [name, setName] = useState('');
-    const [age, setAge] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const history = useHistory();
-    const handleSignup = async () => {
-        let data = await fetch("https://kidcoder.herokuapp.com/signUp",{
-            method:'POST',
-            body: JSON.stringify({name, age, email, password}),
-            headers: {
-                ACCESS_TOKEN_SECRET: 
-                    "0f5cce9faab589e2e5819c498262907ed7a9729953eee56e0d32645693c2d4df0a61314b7d2f1fe354827e4c8b1984b19bb1c7fd8558e67aa2b2a74353b59ef0",
-                    'Content-Type': 'application/json',
-            },   
-        })
-        data = await data.json();
-        console.log(data);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const history = useHistory();
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    const valid = handleClick();
+    if (valid) return;
+    const data = await fetch("https://kidcoder.herokuapp.com/signUp", {
+      method: "POST",
+      body: JSON.stringify({
+        name: name,
+        age: age,
+        email: email,
+        pwd: password,
+      }),
+      headers: {
+        ACCESS_TOKEN_SECRET:
+          "0f5cce9faab589e2e5819c498262907ed7a9729953eee56e0d32645693c2d4df0a61314b7d2f1fe354827e4c8b1984b19bb1c7fd8558e67aa2b2a74353b59ef0",
+        "Content-Type": "application/json",
+      },
+    });
+    const res = await data.json();
+    console.log(data);
+    if (res.status === "success") {
+      console.log("success");
+      history.push("/profile");
     }
-    handleSignup();
-    
+    if (res.status === "error") {
+      console.log("error");
+    }
+  };
 
-    const handleClick = () =>{
-        const name = document.getElementById("input1").value;
-        const age = document.getElementById("input2").value;
-        const email = document.getElementById("input3").value;
-        const password = document.getElementById("input4").value;
-        if(name === '' || age === '' || email === '' || password === ''){
-            alert("Fill Input");
-        }
-        else if(age < 9){
-            return <span>Minimum required age is 9</span>
-        }
-        else if(age > 13){
-            return <span>Maximum required age is 13</span>
-        }
-        else{
-            history.push("/profile");
-        }
+  const handleClick = () => {
+    const name = document.getElementById("input1").value;
+    const age = document.getElementById("input2").value;
+    const email = document.getElementById("input3").value;
+    const password = document.getElementById("input4").value;
+    if (name === "" || age === "" || email === "" || password === "") {
+      alert("Fill Input");
+    } else if (age < 9) {
+      return <span>Minimum required age is 9</span>;
+    } else if (age > 13) {
+      return <span>Maximum required age is 13</span>;
+    } else {
+      history.push("/profile");
     }
-    return ( 
-        <div className="sign">
-            <img className="mascot-doc" src="./images/mascot-doc.png" alt="mascot-doc" />
-            <form className="input-group signup" onSubmit={handleSubmit(onSubmit)}>
-                <label className="label">{t('name')}</label>
-                <input 
-                    type="text"
-                    className="input-field"
-                    placeholder={t('enter-name')}
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
-                    id="input1"
-                    required
-                />
-                <label className="label">{t('age')}</label>
-                <input
-                    type="text"
-                    className="input-field"
-                    placeholder={t('enter-age')}
-                    {...register("age", {
-                        reqired: "Age is required",
-                        min: {
-                            value: 9,
-                            message: "Minimum required age is 9" 
-                        },
-                        max: {
-                            value: 13,
-                            message: "Maximum required age is 13"
-                        },
-                        pattern: {
-                            value: /^[0-9]*$/,
-                            message: "Only numbers are allowed"
-                        }
-                    })}
-                    onChange={(e) => setAge(e.target.value)}
-                    value={age}
-                    id="input2" />
-                <div className='error'>
-                    {errors.age && (<small>{errors.age.message}</small>)}
-                </div><br />
-                <label className="label">{t('email')}</label>
-                <input
-                    type="email"
-                    className="input-field"
-                    placeholder={t('enter-email')}
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
-                    id="input3"
-                    required
-                />
-                <label className="label">{t('your-password')}</label>
-                <input
-                    type={passwordEye === false ? "password": "text"}
-                    className="input-field"
-                    placeholder={t('enter-password')}
-                    onChange={(e) => setPassword(e.target.value)}
-                    value={password}
-                    id="input4"
-                    required
-                />
-                <div className="icon-eye">
-                    {
-                        (passwordEye === false)?<AiFillEyeInvisible onClick={handlePasswordClick} />:
-                        <AiFillEye onClick={handlePasswordClick} />
-                    }  
-                </div>
-                <button type="submit" className="submit-btn btn2" onClick={handleClick}>{t('signup')}</button>
-                <p>{t('already')}<Link to="/signin" className="link">{t('signin')}</Link></p>
-            </form>
+  };
+  return (
+    <div className="sign">
+      <img
+        className="mascot-doc"
+        src="./images/mascot-doc.png"
+        alt="mascot-doc"
+      />
+      <form className="input-group signup" onSubmit={handleSignup}>
+        <label className="label">{t("name")}</label>
+        <input
+          type="text"
+          className="input-field"
+          placeholder={t("enter-name")}
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          id="input1"
+          required
+        />
+        <label className="label">{t("age")}</label>
+        <input
+          type="text"
+          className="input-field"
+          placeholder={t("enter-age")}
+          {...register("age", {
+            reqired: "Age is required",
+            min: {
+              value: 9,
+              message: "Minimum required age is 9",
+            },
+            max: {
+              value: 13,
+              message: "Maximum required age is 13",
+            },
+            pattern: {
+              value: /^[0-9]*$/,
+              message: "Only numbers are allowed",
+            },
+          })}
+          onChange={(e) => setAge(e.target.value)}
+          value={age}
+          id="input2"
+        />
+        <div className="error">
+          {errors.age && <small>{errors.age.message}</small>}
         </div>
-     );
-}
+        <br />
+        <label className="label">{t("email")}</label>
+        <input
+          type="email"
+          className="input-field"
+          placeholder={t("enter-email")}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          id="input3"
+          required
+        />
+        <label className="label">{t("your-password")}</label>
+        <input
+          type={passwordEye === false ? "password" : "text"}
+          className="input-field"
+          placeholder={t("enter-password")}
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          id="input4"
+          required
+        />
+        <div className="icon-eye">
+          {passwordEye === false ? (
+            <AiFillEyeInvisible onClick={handlePasswordClick} />
+          ) : (
+            <AiFillEye onClick={handlePasswordClick} />
+          )}
+        </div>
+        <button type="submit" className="submit-btn btn2">
+          {t("signup")}
+        </button>
+        <p>
+          {t("already")}
+          <Link to="/signin" className="link">
+            {t("signin")}
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
 export default SignUp;
